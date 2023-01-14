7d4e807a3445395008fa0759de5734935796d963
acegoal07 <acegoal07@gmail.com>
2023-01-14 14:40:49 +0000

    1.0.4

--- lib/ProgressBarBuilder.d.ts
+++
@@ -1,9 +0,0 @@
-export function ProgressBarBuilder(settings?: {
-    end: number;
-    position: number;
-    size: number;
-    line: string;
-    slider: string;
-    hideInfo: boolean;
-    percentageInfo: boolean;
-}): Promise<string>;
--- lib/TimeStampBarBuilder.d.ts
+++
@@ -1,9 +0,0 @@
-export function TimeStampBarBuilder(settings?: {
-    posTimeStamp: number;
-    durTimeStamp: number;
-    size: number;
-    line: string;
-    slider: string;
-    hideInfo: boolean;
-    percentageInfo: boolean;
-}): Promise<string>;
